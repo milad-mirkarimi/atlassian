56b11a9ff076c776ef86cfe6518350f27052d396
milad-mirkarimi <themiladmirkarimi@gmail.com>
2023-04-17 13:48:47 +1200

    init commit

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+class javascriptTest {
+	displayDate() {
+		var date = new Date()
+		console.log(date.toLocaleString())
+	}
+}
+
+var myJavascriptTest = new javascriptTest();
+myJavascriptTest.displayDate();
+
+
